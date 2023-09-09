e4525cb1cb4bf97b060e92cfadb1e1ac4f300816
Hemi_bk15 <Hemi_bk15@STHQ0118-07>
2023-09-09 17:45:42 +0400

    Add GetAll to Product controller

--- Presentation/eShop.API/Controllers/ProductController.cs
+++ Presentation/eShop.API/Controllers/ProductController.cs
@@ -1,21 +1,38 @@
-﻿using eShop.Application.Repositories.ProductRepository;
-using eShop.Persistance.Repositories.ProductRepository;
+﻿using eShop.Application.Paginations;
+using eShop.Application.Repositories.ProductRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eShop.API.Controllers;
 
 [Route("api/[controller]")]
-[ApiController]
 public class ProductController : ControllerBase
 {
     private readonly IProductReadRepository _productReadRepository;
     private readonly IProductWriteRepository _productWriteRepository;
 
-    public ProductController(IProductReadRepository productReadRepository,IProductWriteRepository productWriteRepository)
+    public ProductController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
     {
         this._productReadRepository = productReadRepository;
         this._productWriteRepository = productWriteRepository;
     }
 
-    
+    [HttpGet("getAll")]
+    public IActionResult GetAll([FromQuery] Pagination pagination)
+    {
+        try
+        {
+            //return Ok(_productReadRepository.GetAll());
+            var products = _productReadRepository.GetAll(tracking: false);
+            var totalCount = products.Count();
+            products = products.OrderBy(p => p.CreatedDate).Skip(pagination.Size * pagination.Page)
+                .Take(pagination.Size).ToList();
+            return Ok(new { products, totalCount });
+        }
+        catch (Exception)
+        {
+            //Logging
+            return BadRequest();
+        }
+    }
+
 }
