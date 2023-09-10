7201c1a391f6c86a345b4e726aff512f9312e259
Hemi_bk15 <Hemi_bk15@STHQ011A-02>
2023-09-10 17:44:32 +0400

    CRUD with UnitOfWork pattern

--- Presentation/eShop.API/Controllers/ProductController.cs
+++ Presentation/eShop.API/Controllers/ProductController.cs
@@ -34,6 +34,22 @@ public class ProductController : ControllerBase
         }
     }
 
+    [HttpGet("GetbyId/{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        try
+        {
+            var product = await _unitOfWork.ProductReadRepository.GetAsync(p=>p.Id.ToString() == id);    
+            if(product is not null)
+                return Ok(product);
+            return NotFound();
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
+
     [HttpPost("Add")]
     public async Task<IActionResult> Add([FromBody]AddProductViewModel viewModel)
     {
@@ -62,6 +78,44 @@ public class ProductController : ControllerBase
         catch (Exception)
         {
             //Logging
+            return BadRequest();
+        }
+    }
+
+    [HttpPut("Update")]
+    public async Task<IActionResult> Update([FromBody] AddProductViewModel viewModel)
+    {
+        try
+        {
+            Product product = new()
+            {
+                Description = viewModel.Description,
+                Stock = viewModel.Stock,
+                Price = viewModel.Price
+            };
+
+            var data = _unitOfWork.ProductWriteRepository.Update(product);
+            await _unitOfWork.SaveChangesAsync();
+            return Ok();
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<IActionResult> Remove([FromQuery]string id)
+    {
+        try
+        {
+            var product = await _unitOfWork.ProductReadRepository.GetAsync(id); 
+            var data = _unitOfWork.ProductWriteRepository.Remove(product);
+            await _unitOfWork.SaveChangesAsync();
+            return Ok();
+        }
+        catch (Exception)
+        {
             return BadRequest();
         }
     }
